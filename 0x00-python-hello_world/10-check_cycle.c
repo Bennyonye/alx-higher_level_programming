a27baf59bdbe4b0101740f955402809183a6a013
Bennyonye <benedictigbukolu@gmail.com>
2023-11-28 21:12:26 +0100

    phython

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,22 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
-
 /**
- * check_cycle - Entry Point
- * Description: Check if signly linked list has cycle in it
- * @list: pointer to head of list
- * Return: 1 for true, 0 for false
+ * check_cycle - check list
+ * @list: list
+ * Return: 1 or  0.
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *iter1;
+	listint_t *iter2;
 
-	while (fast && fast->next)
+	iter1 = list;
+	iter2 = list;
+	if (list == NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		return (0);
+	}
+	iter2 = iter2->next;
+	while (iter2 != NULL && iter2->next != NULL && iter1 != NULL)
+	{
+		if (iter1 == iter2)
+		{
 			return (1);
+		}
+		iter1 = iter1->next;
+		iter2 = iter2->next->next;
 	}
 	return (0);
 }
