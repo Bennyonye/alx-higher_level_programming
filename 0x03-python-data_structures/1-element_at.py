9cce6bcefe9e601b8e3b497b1106b70414938cc1
Bennyonye <benedictigbukolu@gmail.com>
2023-12-03 22:12:58 +0100

    python

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-    """
-    Retrieves an element from a list.
-
-    Args:
-        my_list (list): The input list.
-        idx (int): The index to retrieve the element.
-
-    Returns:
-        The element at the specified index, or None if the index is out of range.
-    """
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
