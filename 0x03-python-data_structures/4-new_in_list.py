e230703f40a9d3ee9e497639f29ebe036b5b5f98
Bennyonye <benedictigbukolu@gmail.com>
2023-12-03 22:13:52 +0100

    python

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """
-    Replaces an element in a list at a specific position without modifying the original list.
-
-    Args:
-        my_list (list): The input list.
-        idx (int): The index at which the element should be replaced.
-        element: The new element to be inserted.
-
-    Returns:
-        list: A new list with the specified replacement.
-    """
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
