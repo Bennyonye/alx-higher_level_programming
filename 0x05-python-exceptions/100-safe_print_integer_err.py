594f44e2b61f98112305b6759c8cd92cb43294eb
Bennyonye <benedictigbukolu@gmail.com>
2023-12-18 21:59:05 +0100

    python

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 import sys
+
 def safe_print_integer_err(value):
     """Prints an integer with "{:d}".format().
 
@@ -16,9 +18,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except TypeError:
-        print("Exception: TypeError", file=sys.stderr)
-        return (False)
-    except ValueError:
-        print("Exception: ValueError", file=sys.stderr)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (False)
