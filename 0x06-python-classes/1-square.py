f129a83cb1a3302915fe88437148fa94f9c4fc1c
Bennyonye <benedictigbukolu@gmail.com>
2023-12-19 20:25:50 +0100

    python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Defines a class square"""
+
+class Square:
+    """ Represents a square"""
+
+    def __init__(self, size):
+        """ Initiate a new square
+        
+        Arge:
+            size (int): The size of the new Square
+        """
+        self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" Defines a class square"""
+
+class Square:
+    """ Represents a square"""
+
+    def __init__(self, size=0):
+        """ Initiate a new square
+
+        Arge:
+            size (int): The size of the new Square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
