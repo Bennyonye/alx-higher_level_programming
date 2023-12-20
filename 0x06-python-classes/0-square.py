68e156d804305b9063d6307bed7f16b81578496c
Bennyonye <benedictigbukolu@gmail.com>
2023-12-20 18:13:17 +0100

    python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Empty class Square that defines a square"""
+""" Defines a class square"""
 
 
 class Square:
-    """Empty class Square that defines a square"""
+    """ Defines a square"""
     pass
